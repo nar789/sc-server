770042b4cb04a5f8fcce8ca607c83c9ae5877e01
nar789 <admin@hume.co.kr>
2026-01-13 23:35:55 +0900

    fix /my-room

--- src/Route.js
+++ src/Route.js
@@ -322,7 +322,7 @@ export class Route {
       console.log("my-room " + uid);
 
       this.connection.query(
-        `SELECT * FROM room JOIN (SELECT roomid, ranking, created FROM lot WHERE uid='${uid}' and (ranking <= 1)) AS lot ON room.id = lot.roomid ORDER BY created DESC`,
+        `SELECT * FROM room JOIN (SELECT roomid, ranking, created, gift FROM lot WHERE uid='${uid}' and (ranking <= 1)) AS lot ON room.id = lot.roomid ORDER BY created DESC`,
         (err, row) => {
           if (err) throw err;
           res.send(row);
